a5d53642ab651c69a773fbeb8ce7885c93125c46
Abhishek-Ananda <103427547+Abhishek-Ananda@users.noreply.github.com>
2023-05-02 01:02:16 +0530

    Add files via upload

---
+++ src/test/java/com/aaacomunity/program/AaacomunityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.aaacomunity.program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaacomunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+      int a = 2;
+	}
+
+}
